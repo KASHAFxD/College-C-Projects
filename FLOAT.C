545755a3924429e97f7fead3c865c2f5b10ecacf
KASHAFxD <115388325+KASHAFxD@users.noreply.github.com>
2022-10-09 14:28:07 +0530

    Add files via upload
    Some timepass projects 😚

---
+++ FLOAT.C
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c,d;
+printf("write first number=");
+scanf("%d",&a);
+printf("\nwrite second number=");
+scanf("%d",&b);
+printf("write third number=");
+scanf("%d",&c);
+d=a+b+c;
+printf("total=%d",d);
+getch();
+}
---
+++ HOMEWORK.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+     float a,b,c,d;
+     printf("enter first decimal value=");
+     scanf("%f",&a);
+     printf("\nenter second decimal value");
+     scanf("%f",&b);
+     printf("\nenter third number=");
+     scanf("%f",&c);
+     d=a+b+c;
+     printf("\nthe sum is%f",d);
+     getch();
+
+}
+/*This Program Is Written By Kashaf xD, Any Student
+ Can Copy It With GIving Credits to me */
